abb11356b9f69fc4fe6a98b2d25a5c3a9d9431fa
David <david@nlsn.nu>
2021-09-14 15:34:45 +0200

    Changed manifest, mainactivity, apiclient and drink

--- Drink_appAndroid/app/src/main/java/com/example/drink_app/MainActivity.kt
+++ Drink_appAndroid/app/src/main/java/com/example/drink_app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.drink_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.drink_app.network.APIClient
+import com.example.drink_app.network.DrinkResponse
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = APIClient.apiService.fetchCharacters("Gin")
+
+        client.enqueue(object : retrofit2.Callback<DrinkResponse>{
+            override fun onResponse(call: Call<DrinkResponse>, response: Response<DrinkResponse>) {
+                if(response.isSuccessful){
+                    Log.d("1", ""+ response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<DrinkResponse>, t: Throwable) {
+                Log.e("MainActivity", "Something went wrong: " + t)
+            }
+
+        })
     }
 }
--- Drink_appAndroid/app/src/main/java/com/example/drink_app/network/Drink.kt
+++ Drink_appAndroid/app/src/main/java/com/example/drink_app/network/Drink.kt
@@ -1,4 +1,14 @@
 package com.example.drink_app.network
 
-class Drink {
-}
+import com.squareup.moshi.Json
+
+data class Drink (
+    @Json(name = "idDrink")
+    val idDrink : String,
+    @Json(name = "strDrink")
+    val strDrink : String,
+    @Json(name = "strDrinkThumb")
+    val strDrinkThumb : String
+)
+
+data class DrinkResponse(@Json(name = "results") val result : List<Drink>)
