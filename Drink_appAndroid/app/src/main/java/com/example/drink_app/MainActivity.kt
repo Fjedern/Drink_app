28ed6850508f4f1de62e871f9eb23c7fa3d82ebd
PalmquistS <s.palmquist@telia.com>
2021-09-17 16:05:39 +0200

    Improved random drink

--- Drink_appAndroid/app/src/main/java/com/example/drink_app/MainActivity.kt
+++ Drink_appAndroid/app/src/main/java/com/example/drink_app/MainActivity.kt
@@ -10,13 +10,20 @@ import retrofit2.Call
 import retrofit2.Response
 
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import coil.load
+import coil.transform.CircleCropTransformation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getRandomDrink()
 
         val btn_inventory: Button = findViewById(R.id.btn_inventory)
         val btn_recepies: Button = findViewById(R.id.btn_recepies)
@@ -33,8 +40,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_random_drink.setOnClickListener {
-            val intent = Intent(this, DrinkRandom::class.java)
-            startActivity(intent)
+            getRandomDrink()
         }
     }
+
+    private fun getRandomDrink() {
+        val tv_main_activity_random_drink_name: TextView =
+            findViewById(R.id.tv_main_activity_random_drink_name)
+        val iv_main_activity_random_image: ImageView =
+            findViewById(R.id.iv_main_activity_random_image)
+
+        val client = APIClient.apiService.fetchRandomDrinks("")
+
+        client.enqueue(object : retrofit2.Callback<DrinkResponse> {
+            override fun onResponse(
+                call: Call<DrinkResponse>,
+                response: Response<DrinkResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("1", "" + response.body())
+
+                    val result = response.body()?.drinks
+                    result?.let {
+                        tv_main_activity_random_drink_name.text = result[0].strDrink
+                        iv_main_activity_random_image.load(result[0].strDrinkThumb) {
+                            transformations(CircleCropTransformation())
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<DrinkResponse>, t: Throwable) {
+
+
+                Log.e("MainActivity", "Something went wrong: " + t)
+            }
+        })
+    }
 }
+
