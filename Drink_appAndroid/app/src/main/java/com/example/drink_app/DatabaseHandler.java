ea6d50d040b1b84915831c434cf33d8501e01e97
Sandra Wahlgren <71629445+Swala@users.noreply.github.com>
2021-09-20 11:36:19 +0200

    update rv list and toolbar

--- Drink_appAndroid/app/src/main/java/com/example/drink_app/DatabaseHandler.java
+++ Drink_appAndroid/app/src/main/java/com/example/drink_app/DatabaseHandler.java
@@ -17,6 +17,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     //public static final String COLUMN_INGREDIENT_ID = "INGREDIENT_ID";
     public static final String COLUMN_INGREDIENT_NAME = "INGREDIENT_NAME";
 
+    //TEST CHECKBOX- also add in createTableStatement
+    //public static final String COLUMN_INGREDIENT_SELECTED = "INGREDIENT_SELECTED";
+
     public DatabaseHandler(@Nullable Context context){
         super(context, "ingredientsDatabase.db", null, 1);
     }
@@ -26,10 +29,10 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String createTableStatement =
                 "CREATE TABLE " + INGREDIENT_TABLE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_INGREDIENT_NAME + " TEXT )";
+                        COLUMN_INGREDIENT_NAME +  " TEXT)";
 
         sqLiteDatabase.execSQL(createTableStatement);
-        System.out.println("i have created database");
+        //System.out.println("i have created database");
 
     }
 
@@ -47,8 +50,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         String queryString = "SELECT * FROM " + INGREDIENT_TABLE;
         SQLiteDatabase database = this.getReadableDatabase();
         Cursor cursor = database.rawQuery(queryString, null);
-
-
 
         if (cursor.moveToFirst()){
             do{
@@ -74,12 +75,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         cv.put(COLUMN_INGREDIENT_NAME, ingredient_name);
 
-        /*cv.put(COLUMN_INGREDIENT_NAME, "Sugar");
-        cv.put(COLUMN_INGREDIENT_NAME, "Vodka");
-        cv.put(COLUMN_INGREDIENT_NAME, "Lime");
-        cv.put(COLUMN_INGREDIENT_NAME, "Tequila");*/
-
-        System.out.println(cv.size());
+        //cv.put(COLUMN_INGREDIENT_NAME, "Sugar");
+        //System.out.println(cv.size());
 
         long insert = db.insert(INGREDIENT_TABLE, null, cv);
         if(insert == -1){
@@ -89,19 +86,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return true;
     }
 
-    public void deleteIngredient(int ingredient_id){
+    public void deleteIngredient(int... ingredient_id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM " + INGREDIENT_TABLE + " WHERE " +
-                "ID = " + ingredient_id;
-        /*Cursor cursor = db.rawQuery(queryString, null);
-        db.rawQuery(queryString, null);
 
-        return false;*/
-        String idToString = String.valueOf(ingredient_id);
-        db.delete(INGREDIENT_TABLE, "ID=?", new String[]{idToString});
+        for(int i: ingredient_id){
+            //System.out.println(i);
+            db.delete(INGREDIENT_TABLE, "ID=?", new String[]{String.valueOf(i)});
+        }
         db.close();
-
     }
-    //TODO delete from database
-    //TODO update database
+
+    //TODO update invdividual ingredient
 }
--- Drink_appAndroid/app/src/main/java/com/example/drink_app/ListAdaptor.kt
+++ Drink_appAndroid/app/src/main/java/com/example/drink_app/ListAdaptor.kt
@@ -3,6 +3,7 @@ package com.example.drink_app
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -19,13 +20,16 @@ class ListAdaptor (var list : List<Ingredient>) : RecyclerView.Adapter<ListAdapt
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+
         holder.itemView.apply {
             val tv_ingredient_id: TextView = findViewById(R.id.tv_ingredient_id)
             val tv_ingredient_name: TextView = findViewById(R.id.tv_ingredient_name)
+            //val checkBox: CheckBox = findViewById(R.id.cb_ingredient)
 
             tv_ingredient_id.text = list[position].id.toString()
             tv_ingredient_name.text = list[position].name
-
+            //checkBox.text = tv_ingredient_name.text
+            //checkBox.setOnCheckedChangeListener(null)
         }
     }
 
